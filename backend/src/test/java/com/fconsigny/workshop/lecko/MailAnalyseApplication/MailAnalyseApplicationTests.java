99d6bb9d1ec200a7064b27be92d7bd1a0c9d7985
Francois CONSIGNY <fconsigny@gmail.com>
2021-10-27 13:55:37 +0100

    [INIT] - Create maven project sources

---
+++ backend/src/test/java/com/fconsigny/workshop/lecko/MailAnalyseApplication/MailAnalyseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fconsigny.workshop.lecko.MailAnalyseApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAnalyseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
